bd016cd6701427916bd0313ee5d6e9ad0cfb64fe
Devin Weaver <suki@tritarget.org>
2013-09-23 12:15:09 -0400

    Adds ico to dist build

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,11 +285,12 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here
             //'_bower_components/jquery/jquery.js',
-            //'favicon.ico',
-            //'apple-touch*.png'
+            // 'favicon.ico',
+            // 'apple-touch*.png'
+            'ico/**/*'
           ],
           dest: '<%= yeoman.dist %>'
         }]
